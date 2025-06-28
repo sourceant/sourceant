3073fd0d6dd87e7dbfd11e7ebd20c3910cf347e6
nfebe <fenn25.fn@gmail.com>
2025-06-28 13:57:24 +0100

    feat: Enhance github token support
    Signed-off-by: nfebe <fenn25.fn@gmail.com>

--- src/config/settings.py
+++ src/config/settings.py
@@ -17,6 +17,7 @@ LLM = os.getenv("LLM", "gemini")
 LOG_DRIVER: str = os.getenv("LOG_DRIVER", "console")
 LOG_FILE: str = os.getenv("LOG_FILE", "sourceant.log")
 GITHUB_SECRET = os.getenv("GITHUB_SECRET")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
 if GITHUB_SECRET is None:
     raise ValueError("GITHUB_SECRET environment variable is not set.")
