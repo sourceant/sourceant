4cf9b26f0db65aa6cef2097a0b4001d0cf25ae0b
nfebe <fenn25.fn@gmail.com>
2025-06-28 21:45:32 +0100

    fix(dev): Correctly load environment variables
    Signed-off-by: nfebe <fenn25.fn@gmail.com>

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sourceant db upgrade head
 pip install -r requirements-dev.txt
-uvicorn src.api.main:app --reload --host 0.0.0.0 --port 8000
+uvicorn src.api.main:app --reload --host 0.0.0.0 --port 8000 --env-file .env
